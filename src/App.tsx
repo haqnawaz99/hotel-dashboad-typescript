8a265eb49545f9be65d3006ecf5215fe7b23e1da
IbnulHasan475 <108236601+IbnulHasan475@users.noreply.github.com>
2023-01-06 11:21:20 +0500

    components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Card from './components/Card';
+import Create from './components/Create';
+import Details from './components/Details';
+import NotFound from './components/NotFound';
 
 import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hotel Dashboard</h1>
-    </div>
+    <Routes>
+      <Route path="/create" element={<Create/>}/>
+      <Route path="/hotels/:id" element={<Details/>}/>
+      <Route path="*" element={<NotFound/>}/>
+      <Route path="/card" element={<Card/>}/>
+    </Routes>
   );
 }
-
 export default App;
